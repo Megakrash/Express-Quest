2148e20db9b36859c908f39744451fdb2450cf54
Jonathan Scattolini <jscattolini@gmail.com>
2022-11-06 14:14:43 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+
+
+const app = express();
+
+const port = process.env.APP_PORT;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+const movieUsers = require("./movieUsers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", movieUsers.getUsers);
+app.get("/api/users/:id", movieUsers.getUsersById);
+
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
