e67cafb528981ebc1290a42fa7c062cd8602a249
Jonathan Scattolini <jscattolini@gmail.com>
2022-11-07 11:45:25 +0100

    Challenge Express 05

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.post("/api/users", movieUsers.postUser);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", movieUsers.updateUser);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", movieUsers.deleteUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
