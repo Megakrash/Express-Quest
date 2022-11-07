19bf05b965f85b2794f670d682b2b393be0538dd
Jonathan Scattolini <jscattolini@gmail.com>
2022-11-07 14:28:37 +0100

    Challenge Express 04bis

--- app.js
+++ app.js
@@ -23,14 +23,23 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieUsers.getUsers);
 app.get("/api/users/:id", movieUsers.getUsersById);
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", movieUsers.postUser);
+// app.post("/api/movies", movieHandlers.postMovie);
+// app.post("/api/users", movieUsers.postUser);
 
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.put("/api/users/:id", movieUsers.updateUser);
+// app.put("/api/movies/:id", movieHandlers.updateMovie);
+// app.put("/api/users/:id", movieUsers.updateUser);
 
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", movieUsers.deleteUser);
+
+const { validateMovie } = require("./validator.js");
+const { validateUser } = require("./validator.js");
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, movieUsers.postUser);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, movieUsers.updateUser);
+
 
 app.listen(port, (err) => {
   if (err) {
