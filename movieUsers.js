22eef798ebe5fcd9f1553633576561c62fa83649
Jonathan Scattolini <jscattolini@gmail.com>
2022-12-12 12:58:30 +0100

    youhhou

--- movieUsers.js
+++ movieUsers.js
@@ -2,7 +2,7 @@ const database = require("./database");
 
 const getUsers = (req, res) => {
 
-    let sql = "select * from users";
+    let sql = "select id, firstname, lastname, email, city, language from users";
     const sqlValues = [];
 
     if (req.query.language != null) {
@@ -33,7 +33,7 @@ const getUsersById = (req, res) => {
 
 
     database
-        .query("select * from users where id = ?", [id])
+        .query("select id, firstname, lastname, email, city, language from users where id = ?", [id])
         .then(([users]) => {
             if (users[0] != null) {
                 res.json(users[0]);
@@ -48,12 +48,12 @@ const getUsersById = (req, res) => {
 };
 
 const postUser = (req, res) => {
-    const { firstname, lastname, email, city, language } = req.body;
+    const { firstname, lastname, email, city, language, hashedPassword } = req.body;
 
     database
         .query(
-            "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-            [firstname, lastname, email, city, language]
+            "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+            [firstname, lastname, email, city, language, hashedPassword]
         )
         .then(([result]) => {
             res.location(`/api/users/${result.insertId}`).sendStatus(201);
